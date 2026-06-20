edf6808df3b79d0fabce0006c878ec31c31ed573
ZeNik77 <nikitkazel@gmail.com>
2026-06-20 15:04:24 +0300

    refactor(frontend): api fetching with react-router's clientLoader and HydrateFallback

--- apps/frontend/app/components/Plots.tsx
+++ apps/frontend/app/components/Plots.tsx
@@ -10,24 +10,23 @@ interface Props {
 const Plots = ({ sensorSize, timestamps, data, error }: Props) => {
   return (
     <>
-      {(timestamps.length === 0 ||
-        data.length === 0 ||
-        sensorSize === 0 ||
-        error.length !== 0) && (
+      {error.length !== 0 && (
         <div className="text-primary-700 font-bold text-2xl lg:text-3xl mx-8 my-4">
-          {error.length === 0 ? "Получаем данные..." : error}
+          {error}
         </div>
       )}
-      {timestamps.length !== 0 &&
-        data.length !== 0 &&
-        sensorSize !== 0 &&
-        error.length === 0 && (
-          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-x-8 px-6 md:px-7 2xl:px-8 gap-y-6 py-6 bg-accent-100 mx-8 my-4 rounded-[10px] shadow-md shadow-accent-200 border border-primary-300">
-            {Array.from({ length: sensorSize }, (_, i) => (
-              <Plot id={i} timestamps={timestamps} sensorData={data[i]} />
-            ))}
-          </div>
-        )}
+      {error.length === 0 && (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-x-8 px-6 md:px-7 2xl:px-8 gap-y-6 py-6 bg-accent-100 mx-8 my-4 rounded-[10px] shadow-md shadow-accent-200 border border-primary-300">
+          {Array.from({ length: sensorSize }, (_, i) => (
+            <Plot
+              key={`plot${i}`}
+              id={i}
+              timestamps={timestamps}
+              sensorData={data[i]}
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 };
