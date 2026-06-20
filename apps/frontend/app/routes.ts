ec4fb72c14ceb0ac896e5881d8ed5a78ad8cf2e8
ZeNik77 <42634950+ZeNik77@users.noreply.github.com>
2026-06-20 21:47:44 +0300

    feat(frontend): responsive pages and sidebar, refactored routes and api fetching (#45)
    * feat: add table overflow behavior
    
    * fix: fixed accent-300 color
    
    * feat(frontend): added plots and tweaked the table head text color
    
    * chore: remove debug output
    
    * fix(frontend): proper error display if failed to fetch
    
    * feat(frontend): most things are responsive
    
    * refactor(frontend): move the responsive sidebar from root.tsx
    
    * feat(backend): example parser, endpoint /plots added
    
    * feat(frontend): responsive table and plots
    
    * fix(frontend): further improve error display
    
    * refactor(frontend): set the routes up for homepage and scalable pages
    
    * refactor(frontend): api fetching with react-router's clientLoader and HydrateFallback
    
    * ci: add code coverage workflow (#42)
    
    * ci: add code coverage workflow
    
    * chore(taskfile): modularize tasks and optimize test execution
    
    - Split Taskfile by domain (frontend, ml)
    - Refactor test tasks to run in parallel with clean output
    - Cache dependencies from lockfiles to speed up setup
    
    * chore(gitignore): add .task to .gitignore
    
    * chore(infra): restructure nginx config and fix compose permissions (#43)
    
    * chore(infra): move nginx.conf to infra/nginx and update prod compose
    
    * fix(compose): resolve react-router volume permission denied
    
    * feat(frontend): plot visualization and table appearance
    
    * feat: add table overflow behavior
    
    * fix: fixed accent-300 color
    
    * feat(frontend): added plots and tweaked the table head text color
    
    * chore: remove debug output
    
    * fix(frontend): proper error display if failed to fetch
    
    * feat(backend): example parser, endpoint /plots added
    
    * feat(backend): example parser, endpoint /plots added
    
    * fix(ML): Delete apps/ml/sample_lemon.png
    
    * feat(frontend): added plots and tweaked the table head text color
    
    * fix(frontend): fixed "npm ci" problem
    
    ---------
    
    Co-authored-by: ratteperk <spered2109@gmail.com>
    
    * fix(frontend): fixed Table.tsx after merging
    
    * chore(frontend): remove merge duplicates
    
    ---------
    
    Co-authored-by: ratteperk <spered2109@gmail.com>
    Co-authored-by: Zagir Latypov <120632527+CreoCot@users.noreply.github.com>

--- apps/frontend/app/routes.ts
+++ apps/frontend/app/routes.ts
@@ -1,6 +1,13 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("/data", "routes/data.tsx"),
+  layout("routes/sidebarpages.tsx", [
+    index("routes/home.tsx"),
+    route("/data", "routes/data.tsx"),
+  ]),
 ] satisfies RouteConfig;
