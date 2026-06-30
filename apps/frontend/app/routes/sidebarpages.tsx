f87387b332208bd1fa86495a2b7f86cb5fbd8462
ZeNik77 <nikitkazel@gmail.com>
2026-06-30 20:46:34 +0300

    fix(frontend): proper data access

--- apps/frontend/app/routes/sidebarpages.tsx
+++ apps/frontend/app/routes/sidebarpages.tsx
@@ -1,17 +1,25 @@
 import { Outlet, redirect } from "react-router";
 import ResponsiveSidebar from "../components/ResponsiveSidebar";
 import api from "../axios";
+import type { LoaderFunctionArgs } from "react-router";
 
-export async function loader() {
+export async function clientLoader() {
   try {
-    await api.get("/auth/me", { skipAuthRedirect: true } as any);
-    return null;
-  } catch {
+    await api.get("/api/v1/auth/me");
+  } catch (error) {
+    if (error instanceof Error) console.log("HERE2", error.message);
     throw redirect("/login");
   }
 }
-
-const sidebarpages = () => {
+export function HydrateFallback() {
+  return (
+    <div className="flex w-full h-full min-h-screen">
+      <ResponsiveSidebar />
+      <div className="w-full overflow-hidden bg-grey-100 pb-5" />
+    </div>
+  );
+}
+export const sidebarpages = () => {
   return (
     <div className="flex w-full h-full min-h-screen">
       <ResponsiveSidebar />
